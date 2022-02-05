073b165a0c0b306173cbafba01d16e8f092b79cf
creative201347 <creative201347@gmail.com>
2022-02-05 13:47:55 +0545

    getting posts from ssr

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,10 @@ import Feed from "../components/Feed";
 import { AnimatePresence } from "framer-motion";
 import { modalState, modalTypeState } from "../atoms/modalAtom";
 import { useRecoilState } from "recoil";
+import { connectToDatabase } from "../util/mongodb";
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts);
   const [modalOpen, setModalOpen] = useRecoilState(modalState);
   const [modalType, setModalType] = useRecoilState(modalTypeState);
 
@@ -48,10 +50,27 @@ export async function getServerSideProps(context) {
       },
     };
   }
+  // Get posts on SSR
+  const { db } = await connectToDatabase();
+  const posts = await db
+    .collection("posts")
+    .find()
+    .sort({ timestamp: -1 })
+    .toArray();
+
+  // Get google news API
 
   return {
     props: {
       session,
+      posts: posts.map((post) => ({
+        _id: post._id.toString(),
+        input: post.input,
+        username: post.username,
+        email: post.email,
+        userImg: post.userImg,
+        createdAt: post.createdAt,
+      })),
     },
   };
 }
