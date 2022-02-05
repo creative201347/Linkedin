ec85d5c26b4bf7b68dbccdb797297b136d9cf72f
creative201347 <creative201347@gmail.com>
2022-02-05 13:23:46 +0545

    retreviewing posts

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
 import Input from "../components/Input";
 import Post from "../components/Post";
 
 const Feed = () => {
+  const [realTimePosts, setRealTimePosts] = useState([]);
+  const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+  const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/posts", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const responseData = await response.json();
+      setRealTimePosts(responseData);
+      setHandlePost(false);
+      setUseSSRPosts(false);
+    };
+    fetchPosts();
+  }, [handlePost]);
+  console.log(realTimePosts);
+
   return (
     <div className="space-y-6 pb-24 max-w-lg">
       <Input />
